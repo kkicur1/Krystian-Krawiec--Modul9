10f2706be04640a070f35b3b0bf3bea423d34a17
Krystian Krawiec <kkicur1@tlen.pl>
2022-04-06 19:15:17 +0200

    init

---
+++ src/main/java/com/kodilla/kodillacourse/Modul9Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modul9Application.class, args);
+    }
+
+}
